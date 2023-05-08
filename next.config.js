bc69b81556bd3145733b09e60c09a19863182d83
mad-IWAD <hurricaneuser@gmail.com>
2023-05-08 14:37:14 +0700

    change plan

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
-  images: {
-    loader: 'custom',
-    loaderFile: './app/image.ts',
-  },
-  experimental: {
-    appDir: true,
-  },
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
  
 module.exports = nextConfig;
